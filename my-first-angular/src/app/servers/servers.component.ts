9db7f337cfb3c2c2969009d33f28240c549a989c
raju <33832428+Knightch@users.noreply.github.com>
2019-06-14 11:10:17 +0530

    using ngif

--- my-first-angular/src/app/servers/servers.component.ts
+++ my-first-angular/src/app/servers/servers.component.ts
@@ -9,15 +9,18 @@ export class ServersComponent implements OnInit {
  serverCreationStatus = 'No server Created'
  serverName = '';
  allowNewServer = false;
+ serverCreated = false;
   constructor() {
   setTimeout(() => {
    this.allowNewServer = true;
   },2000);
+
    }
 
   ngOnInit() {
   }
  onCreateServer(){
+  this.serverCreated = true;
   this.serverCreationStatus='server is created! name is '+ this.serverName;
  }
  onUpdateServerName(event:Event){
